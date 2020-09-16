cea031dcb16e4f6ce0403f99757d7099ef05d40f
Alvaro Montenegro <aamontenegros@unal.edu.co>
2020-09-16 14:10:13 -0500

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,71 @@
+/*
+	Fractal by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body');
+
+	// Breakpoints.
+		breakpoints({
+			xlarge:   [ '1281px',  '1680px' ],
+			large:    [ '981px',   '1280px' ],
+			medium:   [ '737px',   '980px'  ],
+			small:    [ '481px',   '736px'  ],
+			xsmall:   [ '361px',   '480px'  ],
+			xxsmall:  [ null,      '360px'  ]
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Mobile?
+		if (browser.mobile)
+			$body.addClass('is-mobile');
+		else {
+
+			breakpoints.on('>medium', function() {
+				$body.removeClass('is-mobile');
+			});
+
+			breakpoints.on('<=medium', function() {
+				$body.addClass('is-mobile');
+			});
+
+		}
+
+	// Scrolly.
+		$('.scrolly')
+			.scrolly({
+				speed: 1500
+			});
+
+	//Carlos Maldonado
+		$('#boton-fullscreen').click((e)=>{
+			var docElement = document.documentElement;
+			
+			var activo = $('#boton-fullscreen').hasClass('fullscreen-activo');
+			if(activo) {
+				document.exitFullscreen = document.exitFullscreen || document.mozCancelFullScreen || document.webkitExitFullscreen;
+				$('#boton-fullscreen').removeClass('fullscreen-activo');
+				document.exitFullscreen();	
+			}
+			else {
+				$('#boton-fullscreen').addClass('fullscreen-activo');
+				docElement.requestFullscreen = docElement.requestFullscreen || docElement.mozRequestFullScreen || docElement.webkitRequestFullscreen || docElement.msRequestFullscreen; 
+				docElement.requestFullscreen();
+			}
+			
+
+		});
+
+
+
+})(jQuery);
